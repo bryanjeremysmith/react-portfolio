8fd2ff86a02d8ad65e19c0a20d6603b1fd994da1
BJ Smith <bryanjeremysmith@gmail.com>
2023-03-31 13:42:58 -0700

    adding skeleton from class

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,15 @@
+import React, {useState } from 'react';
+import Project from "../Project";
+//import Calcul
+
+function Portfolio() {
+    const [projects] = useState([
+        {
+            name: '',
+            description: '',
+            link: "https://github.com",
+            repo: "https://github.com",
+            //imgSrc: Calculator
+        }
+    ])
+}
