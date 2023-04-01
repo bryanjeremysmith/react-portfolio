16184412d507d4a13bdad4822743d7cc4427a172
BJ Smith <bryanjeremysmith@gmail.com>
2023-04-01 14:33:05 -0700

    adding contact information

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+// import React, { useState } from "react";
+
+function Contact() {
+	return (
+		<section id="contact-me" class="mycard">
+			<section class="card-container">
+					<header>Contact Me</header>
+					<a href="tel:5558675309">Call me at 555-867-5309</a>
+					<a href="mailto:bryanjeremysmith@gmail.com?subject = Email about Portfolio&body = I found you through GitHub">Email me</a>
+					<a href="https://www.linkedin.com/in/bryanjeremysmith/">LinkedIn</a>
+					<a href="https://github.com/bryanjeremysmith">GitHub</a>
+			</section>
+		</section>
+	);
+}
+
+export default Contact;
