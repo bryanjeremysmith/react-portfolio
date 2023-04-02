a1d0b85f91e9b6a091517cf262b694df4b92577a
BJ Smith <bryanjeremysmith@gmail.com>
2023-04-02 11:49:20 -0700

    adding project

---
+++ src/components/Project/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+
+function Project(props) {
+	// This imports the information for the current project sent down.
+	const currentProject = useState(props)[0].projects;
+
+	const name = currentProject.name;
+	const description = currentProject.description;
+	const link = currentProject.link;
+	const repo = currentProject.repo;
+	const imgSrc = currentProject.imgSrc;
+
+	return (
+		<Card style={{ width: "18rem" }}>
+			<Card.Img
+				variant="top"
+				src={require({imgSrc})}
+				className="card-image"
+			/>
+			<div className="center">
+				<Card.Body>
+					<Card.Name className="card-name">{name}</Card.Name>
+					<Card.Text className="card-description">{description}</Card.Text>
+					<Card.Link href={link} target="_blank" className="card-link">
+						{name} App
+					</Card.Link>
+					<Card.Link href={repo} target="_blank" className="card-repo">
+						{name} Github
+					</Card.Link>
+				</Card.Body>
+			</div>
+		</Card>
+	);
+}
+
+export default Project;
