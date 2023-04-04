0cb70c182fe9d5ad7963c362ef186102d96527da
BJ Smith <bryanjeremysmith@gmail.com>
2023-04-04 14:09:17 -0700

    adding image linking to URL

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -9,24 +9,31 @@ function Project(props) {
 	const description = currentProject.description;
 	const image = currentProject.image;
 	const repo = currentProject.repo;
+	const deploy = currentProject.deploy;
 
 	return (
+
 		<Card style={{ width: "18rem" }}>
-			<Card.Img
-				variant="top"
-				src={require(`../../assets/images/${image}`)}
-				className="card-image"
-				href={repo}
-			/>
-			<div className="center">
-				<Card.Body>
-					<Card.Title className="card-title">{name}</Card.Title>
-					<Card.Text className="card-text">{description}</Card.Text>
-					<Card.Link href={repo} target="_blank" className="card-link">
-						{name}
-					</Card.Link>
-				</Card.Body>
-			</div>
+			<Card.Link href={deploy}>
+				<Card.Img
+					variant="top"
+					src={require(`../../assets/images/${image}`)}
+					className="card-image"
+				/>
+				<div className="center">
+					<Card.Body>
+						<Card.Title className="card-title">{name}</Card.Title>
+						<Card.Text className="card-text">{description}</Card.Text>
+						<Card.Link href={repo} target="_blank" className="card-link">
+							{name} GitHub Repo
+						</Card.Link>
+						<br />
+						<Card.Link href={deploy} target="_blank" className="card-link">
+							{name} Live Deployment
+						</Card.Link>
+					</Card.Body>
+				</div>
+			</Card.Link>
 		</Card>
 	);
 }
