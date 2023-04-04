960c68f84a307424a21e94ac62d3ea7ab730065a
BJ Smith <bryanjeremysmith@gmail.com>
2023-04-04 11:16:57 -0700

    removing redundancy

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,9 +3,6 @@ import React from "react";
 function Resume() {
 	return (
 		<section>
-			<div className="center">
-				<h1 className="page-header">My Resume</h1>
-			</div>
 			<div>
 				<a href="https://www.linkedin.com/in/bryanjeremysmith/">
 					LinkedIn Profile
