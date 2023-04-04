92efc762dae903a61d0713dc76a20f0d129de00b
BJ Smith <bryanjeremysmith@gmail.com>
2023-04-04 10:15:49 -0700

    updating navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,15 +5,15 @@ function Navigation(props) {
 
 	return (
 		<nav>
-			<ul className="flex-row mobile-view">
-				<li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
-					<span onClick={() => setCurrentTab("about")}>About Me</span>
+			<ul className="flex-row">
+				<li className={currentTab === "about me" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("about me")}>About Me</span>
 				</li>
 				<li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}>
 					<span onClick={() => setCurrentTab("portfolio")}>Portfolio</span>
 				</li>
-				<li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}>
-					<span onClick={() => setCurrentTab("contact")}>Contact</span>
+				<li className={currentTab === "contact me" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("contact me")}>Contact</span>
 				</li>
 				<li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}>
 					<span onClick={() => setCurrentTab("resume")}>Resume</span>
