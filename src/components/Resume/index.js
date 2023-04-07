a9f45fed74a2f2dfced61fe64e4f7e33ec50a134
BJ Smith <bryanjeremysmith@gmail.com>
2023-04-06 20:35:47 -0700

    updated proficiencies

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,9 +4,19 @@ function Resume() {
 	return (
 		<section>
 			<div>
-				<a href="https://www.linkedin.com/in/bryanjeremysmith/">
-					LinkedIn Profile
-				</a>
+				<h4>I am proficient in</h4>
+				<ol>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JavaScript</li>
+					<li>jQuery</li>
+					<li>React</li>
+					<li>Git</li> 
+					<li>MongoDB</li>
+					<li>Express</li>
+					<li>REST</li>
+					<li>NodeJS</li>
+				</ol>
 			</div>
 			<div>
 				<a href={require("../../assets/files/Resume.pdf")} download>
